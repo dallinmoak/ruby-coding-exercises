c3321276fa2efb1a2c855503afa08579439323e6
dallinmoak <dallinmoak@gmail.com>
2018-01-24 09:30:25 -0700

    my solution plus the better one from jordan

--- december/30.rb
+++ december/30.rb
@@ -1,9 +1,21 @@
 require 'rspec'
 
+class Hash
+  def param_converter
+    arr = []
+    self.each do |key, value|
+      arr.push("#{key}=#{value}")
+    end
+    arr.join("&")
+  end
+end
+
+#better solution:
+# self.map {|i| i * "="} * "&"
+
 describe 'HTML Param Converter' do
   it 'Adds an HTML param converter to the Hash class' do
     hash = { :topic => "baseball", :team => "astros" }
     expect(hash.param_converter).to eq('topic=baseball&team=astros')
   end
 end
-
