eb7e45d8ea8f84ad613185c6563ea2e8f75d7371
dallinmoak <dallinmoak@gmail.com>
2018-01-31 09:47:04 -0700

    my solution and jordan's

--- january/2.rb
+++ january/2.rb
@@ -1,19 +1,25 @@
 require 'rspec'
 
-# <!doctype html>
 
-# <html lang="en">
-# <head>
-#   <meta charset="utf-8">
-# 
-#   <title>Title Goes Here</title>
-# </head>
-# 
-# <body>
-# </body>
-# </html>
 
 def html_generator title
+  # #Jordan's solution
+  # <<~HTML
+  #   <!doctype html>
+  #
+  #   <html lang="en">
+  #   <head>
+  #     <meta charset="utf-8">
+  #
+  #     <title>#{title}</title>
+  #   </head>
+  #
+  #   <body>
+  #   </body>
+  #   </html>
+  # HTML
+
+  str ="<!doctype html>\n<html lang=\"en\">\n<head>\n\t<meta charset=\"utf-8\">\n\n\t<title>#{title}</title>\n</head>\n\n<body>\n</body>\n</html>"
 end
 
 describe 'HTML generator' do
