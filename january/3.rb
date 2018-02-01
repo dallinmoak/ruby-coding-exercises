c8aed7775c9648d0b4b84c783e3e69d01cb4ac00
dallinmoak <dallinmoak@gmail.com>
2018-02-01 10:07:35 -0700

    my solution, adaptation of jordans

--- january/3.rb
+++ january/3.rb
@@ -1,10 +1,23 @@
 require 'rspec'
 
 class String
+
+  def clean_up
+    #Jordan's method:
+    arr = self.scan(/\w+/)
+    arr.join(" ")
+  end
+
   def total_words
+    self.clean_up.split.count
   end
 
   def word_list
+    list = Hash.new(0)
+    self.downcase.clean_up.split.each do |word|
+      list[word] += 1
+    end
+    list
   end
 end
 
@@ -18,14 +31,14 @@ describe 'Word Reporter' do
   end
 
   it 'Returns a hash that totals up word usage' do
-    expect(@str.word_list).to eq({"the"=>2,
-                                  "quick"=>1,
-                                  "brown"=>1,
-                                  "fox"=>2,
-                                  "jumped"=>1,
-                                  "over"=>1,
-                                  "lazy"=>1}
-                                )
+    expect(@str.word_list).to eq({
+      "the"=>2,
+      "quick"=>1,
+      "brown"=>1,
+      "fox"=>2,
+      "jumped"=>1,
+      "over"=>1,
+      "lazy"=>1
+      })
   end
 end
-
