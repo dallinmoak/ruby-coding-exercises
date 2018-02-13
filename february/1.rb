78a3bd79db6380d6c70c7c4e7f7a30cc647abbf3
dallinmoak <dallinmoak@gmail.com>
2018-02-13 09:55:14 -0700

    my solution

--- february/1.rb
+++ february/1.rb
@@ -1,6 +1,22 @@
 require 'rspec'
 
+# class LazyObject < ::BasicObject
+#   def initialize(&callable)
+#     @callable = callable
+#   end
+
+#   def __target_object__
+#     @__target_object__ ||= @callable.call
+#   end
+
+#   def method_missing(method_name, *args, &block)
+#     __target_object__.send(method_name, *args, &block)
+#   end
+# end
+
 def big_data_parser num
+  # (1..num).lazy.map{|i| i ** 2 }.first(5)
+  num.times.lazy.map {|n| (n+1) **2}.first(5)
 end
 
 describe 'Big Data' do
